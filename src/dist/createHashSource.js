8e80046f0dd34668408a24073427e22ac6c7fb58
Will Stone <654103+will-stone@users.noreply.github.com>
2019-04-01 15:42:36 +0100

    fix: should compile to es5

--- src/dist/createHashSource.js
+++ src/dist/createHashSource.js
@@ -1,44 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const getHashPath = () => {
-    const href = window.location.href;
-    const hashIndex = href.indexOf('#');
+var getHashPath = function () {
+    var href = window.location.href;
+    var hashIndex = href.indexOf('#');
     return hashIndex === -1 ? '' : href.substring(hashIndex + 1);
 };
-const pushHashPath = (path) => (window.location.hash = path);
-const replaceHashPath = (path) => {
-    const hashIndex = window.location.href.indexOf('#');
+var pushHashPath = function (path) { return (window.location.hash = path); };
+var replaceHashPath = function (path) {
+    var hashIndex = window.location.href.indexOf('#');
     window.location.replace(window.location.href.slice(0, hashIndex >= 0 ? hashIndex : 0) + '#' + path);
 };
-const getState = (path) => {
-    const pathname = path ? path : getHashPath();
-    return { pathname, search: '' };
+var getState = function (path) {
+    var pathname = path ? path : getHashPath();
+    return { pathname: pathname, search: '' };
 };
-const resolveInitialState = (state) => {
+var resolveInitialState = function (state) {
     if (state.pathname === '') {
         replaceHashPath('/');
     }
 };
-const createHashSource = () => {
-    let state = getState();
+var createHashSource = function () {
+    var state = getState();
     resolveInitialState(state);
     return {
         get location() {
             return getState();
         },
-        addEventListener(name, fn) {
+        addEventListener: function (name, fn) {
             window.addEventListener(name, fn);
         },
-        removeEventListener(name, fn) {
+        removeEventListener: function (name, fn) {
             window.removeEventListener(name, fn);
         },
         history: {
-            state,
-            pushState(stateObj, _, uri) {
+            state: state,
+            pushState: function (stateObj, _, uri) {
                 state = getState(uri);
                 pushHashPath(uri);
             },
-            replaceState(stateObj, _, uri) {
+            replaceState: function (stateObj, _, uri) {
                 state = getState(uri);
                 replaceHashPath(uri);
             },
